6901c6ee4de635a4af281d5be8857f2b5c9c4380
mobigaurav <meetgaurav1@gmail.com>
2024-04-02 15:14:46 -0400

    updated changes

--- src/providers/ApolloClientProvider.tsx
+++ src/providers/ApolloClientProvider.tsx
@@ -5,7 +5,7 @@ const client = new ApolloClient({
   uri: 'https://kualaterengganu.stepzen.net/api/brawny-shrimp/__graphql',
   headers: {
     Authorization:
-      'apikey kualaterengganu::stepzen.net+1000::f3ae3429ad14309f3fe6029e8c80368d99ecf16a732c3151d787539dea0c394b',
+      'ApiKey',
   },
   cache: new InMemoryCache(),
 });
